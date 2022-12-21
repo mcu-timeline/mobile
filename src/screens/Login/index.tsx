c9db804f3583686a662d38a2667862a70d1fb388
Adam Gołąb <edek150@gmail.com>
2022-12-21 13:23:07 +0100

    Style login screen (#4)

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,25 @@
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 import { Layout } from '../../components/Layout';
 import { LoginForm } from '../../components/LoginForm';
 
+const styles = StyleSheet.create({
+  backgroundContainer: { position: 'absolute', width: '100%', height: '75%', top: 0 },
+  backgroundImage: { resizeMode: 'cover', height: '133%', top: undefined },
+  gradient: { position: 'absolute', width: '100%', height: '35%', bottom: 0 },
+});
+
 export const LoginScreen = () => (
-  <Layout withMenu>
-    <LoginForm />
-  </Layout>
+  <View style={{ flex: 1 }}>
+    <ImageBackground
+      style={styles.backgroundContainer}
+      imageStyle={styles.backgroundImage}
+      source={require('../../assets/images/background.jpg')}
+    />
+    <LinearGradient colors={['transparent', '#000000']} locations={[0, 0.25]} style={styles.gradient} />
+    <Layout>
+      <LoginForm />
+    </Layout>
+  </View>
 );
