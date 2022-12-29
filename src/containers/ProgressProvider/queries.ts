52f56d87969dd9c678dcf3e2ac7f95c85bc1f8b7
Adam Gołąb <adam.golab@pickatale.com>
2022-12-29 14:32:20 +0100

    fix: move setting active timeline to the progress provider

---
+++ src/containers/ProgressProvider/queries.ts
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+
+export type UserProgress = {
+  id: string;
+  userId: string;
+  activeTimeline: string | null;
+  currentMovieId: string | null;
+};
+
+export type Response<Key extends string> = {
+  [key in Key]: UserProgress;
+};
+
+export const GET_PROGRESS = gql`
+  query {
+    getUserProgress {
+      id
+      userId
+      activeTimeline
+      currentMovieId
+    }
+  }
+`;
+
+export const UPDATE_CURRENT_MOVIE = gql`
+  mutation ($data: UpdateCurrentMovieInput) {
+    updateCurrentMovie(updateCurrentMovieInput: $data) {
+      id
+      userId
+      activeTimeline
+      currentMovieId
+    }
+  }
+`;
+
+export type UpdateCurrentMovieParams = {
+  data: {
+    activeTimelineId: string;
+    currentMovieId: string;
+  };
+};
+
+export const UPSERT_ACTIVE_TIMELINE = gql`
+  mutation ($data: UpsertActiveTimelineInput) {
+    upsertActiveTimeline(upsertActiveTimelineInput: $data) {
+      id
+      userId
+      activeTimeline
+      currentMovieId
+    }
+  }
+`;
+
+export type UpsertActiveTimelineParams = {
+  data: {
+    activeTimelineId: string;
+  };
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,3 @@
 import { FC, ReactNode } from 'react';
 
 export type FCC<T extends {} = {}> = FC<T & { children: ReactNode }>;
-
-export type UserProgress = {
-  id: string;
-  userId: string;
-  activeTimeline: string | null;
-  currentMovieId: string | null;
-};
