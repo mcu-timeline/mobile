2773f7e186edfe23cbbaeddea5112fba54e445d7
Adam Gołąb <edek150@gmail.com>
2023-01-30 16:39:30 +0100

    feat: remove theme provider

--- src/hooks/useTheme.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { ThemeContext } from '../containers/ThemeProvider/context';
-
-export const useTheme = () => useContext(ThemeContext);
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,16 @@ import { Auth0Provider } from 'react-native-auth0';
 import { ApolloClientProvider } from './containers/ApolloClient';
 import { AuthContextProvider } from './containers/AuthProvider';
 import { ProgressContextProvider } from './containers/ProgressProvider';
-import { ThemeProvider } from './containers/ThemeProvider';
 import { Router } from './screens';
 
 export const App = () => (
-  <ThemeProvider>
-    <Auth0Provider domain={'franchise-tracker.eu.auth0.com'} clientId={'TbimV7cIUwYIcg6apLwVC3wsgAEh6rWs'}>
-      <AuthContextProvider>
-        <ApolloClientProvider>
-          <ProgressContextProvider>
-            <Router />
-          </ProgressContextProvider>
-        </ApolloClientProvider>
-      </AuthContextProvider>
-    </Auth0Provider>
-  </ThemeProvider>
+  <Auth0Provider domain={'franchise-tracker.eu.auth0.com'} clientId={'TbimV7cIUwYIcg6apLwVC3wsgAEh6rWs'}>
+    <AuthContextProvider>
+      <ApolloClientProvider>
+        <ProgressContextProvider>
+          <Router />
+        </ProgressContextProvider>
+      </ApolloClientProvider>
+    </AuthContextProvider>
+  </Auth0Provider>
 );
