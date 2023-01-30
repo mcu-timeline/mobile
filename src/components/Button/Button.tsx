157c66cc6755c54e64d301b2e1860e358c6a715d
Adam Gołąb <edek150@gmail.com>
2023-01-30 16:40:42 +0100

    feat: add image button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react';
+import { Pressable, PressableProps, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native';
+
+type Props = PressableProps & {
+  children: string;
+  variant: 'primary' | 'secondary';
+  style?: StyleProp<ViewStyle>;
+};
+
+const commonStyles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 35,
+    margin: 10,
+    height: 70,
+    width: '80%',
+  },
+});
+
+const styles = {
+  button: StyleSheet.create({
+    primary: {
+      ...commonStyles.button,
+      backgroundColor: '#ffffff50',
+    },
+    secondary: {
+      ...commonStyles.button,
+      borderColor: '#ffffff50',
+      borderWidth: 3,
+    },
+  }),
+  text: StyleSheet.create({
+    primary: {
+      fontSize: 20,
+      letterSpacing: 0.25,
+      color: '#ffffff',
+    },
+    secondary: {
+      fontSize: 15,
+      letterSpacing: 0.25,
+      color: '#ffffff',
+    },
+  }),
+};
+
+export const Button: FC<Props> = ({ children, variant, style, ...props }) => {
+  return (
+    <Pressable {...props} style={[styles.button[variant], style]}>
+      <Text style={styles.text[variant]}>{children}</Text>
+    </Pressable>
+  );
+};
---
+++ src/components/Button/ImageButton.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import {
+  Pressable,
+  PressableProps,
+  Text,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+  Image,
+  ImageSourcePropType,
+} from 'react-native';
+
+type Props = PressableProps & {
+  style?: StyleProp<ViewStyle>;
+  imageSource: ImageSourcePropType;
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 35,
+    margin: 10,
+    height: 70,
+    width: 70,
+  },
+  image: {
+    height: '100%',
+    aspectRatio: 1,
+    borderRadius: 35,
+  },
+});
+
+export const ImageButton: FC<Props> = ({ children, style, imageSource, ...props }) => {
+  return (
+    <Pressable {...props} style={[styles.button, style]}>
+      <Image source={imageSource} style={styles.image} />
+    </Pressable>
+  );
+};
