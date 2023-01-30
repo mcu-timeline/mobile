b9a835fbd94017b95985b29347b520418bbec3f3
Adam Gołąb <edek150@gmail.com>
2023-01-30 16:43:27 +0100

    feat: optimise styles after removing theme provider

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,25 +1,23 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import { useTheme } from '../../hooks';
 
 type Props = {
   children: string;
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '90%',
+    marginVertical: 20,
+    alignSelf: 'center',
+  },
+  line: { flex: 1, height: StyleSheet.hairlineWidth, backgroundColor: '#ffffff' },
+  text: { textAlign: 'center', color: '#ffffff', marginHorizontal: 20 },
+});
+
 export const Separator: FC<Props> = ({ children }) => {
-  const colorPalette = useTheme();
-
-  const styles = useMemo(
-    () =>
-      StyleSheet.create({
-        container: { flexDirection: 'row', alignItems: 'center', width: '90%', marginVertical: 20 },
-        line: { flex: 1, height: StyleSheet.hairlineWidth, backgroundColor: colorPalette.text },
-        text: { textAlign: 'center', color: colorPalette.text, marginHorizontal: 20 },
-      }),
-    [colorPalette],
-  );
-
   return (
     <View style={styles.container}>
       <View style={styles.line} />
