40fd3695dc074e2202949cfd0a83f8582e00554b
Adam Gołąb <edek150@gmail.com>
2023-01-30 16:42:40 +0100

    feat: add tag component

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+import { FCC } from '../../types';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffffff50',
+    borderRadius: 20,
+    padding: 10,
+  },
+  text: {
+    color: '#ffffff',
+  },
+});
+
+type Props = { children: string; gap: number };
+
+export const Tag: FCC<Props> = ({ children, gap }) => (
+  <View style={{ ...styles.container, marginHorizontal: gap }}>
+    <Text style={styles.text}>{children}</Text>
+  </View>
+);
